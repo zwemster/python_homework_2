1073b0d81a87f8fc0fff07a5e53a7a83950e48a8
maxim_stoma <maksim.stoma@mail.ru>
2022-12-11 19:27:45 +0300

    task000 edited, file task001.py created

--- task000.py
+++ task000.py
@@ -1,10 +1,6 @@
-from random import randint
-
-random_list = [randint(-10, 10) for i in range(int(input('Сколько чисел забить в список? ')))]
-print(random_list)
 summary = 0
-
-for i in range(len(random_list)):
+array = list(map(int, input('Введи сколь угодно чисел: ').split()))
+for i in range(len(array)):
     if i % 2 != 0:
-        summary += random_list[i]
-print(f'Сумма элементов на нечётных позициях списка равна {summary}')
+        summary += array[i]
+print(f'Сумма элементов на нечётных позициях {summary}')
